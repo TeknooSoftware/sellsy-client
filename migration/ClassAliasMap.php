b52e4c0821f55849d56648ae0ab70ea7c0657dd1
Richard Deloge <r.deloge@uni-alteri.com>
2015-10-27 22:34:29 +0100

    Migrate from Uni Alteri Organization to Teknoo Software

---
+++ migration/ClassAliasMap.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'UniAlteri\Sellsy\Client\ClientInterface' => 'Teknoo\Sellsy\Client\ClientInterface',
+    'UniAlteri\Sellsy\Client\ClientGenerator' => 'Teknoo\Sellsy\Client\ClientGenerator',
+    'UniAlteri\Sellsy\Client\Client' => 'Teknoo\Sellsy\Client\Client',
+    'UniAlteri\Sellsy\Client\Exception\RequestFailureException' => 'Teknoo\Sellsy\Client\Exception\RequestFailureException',
+    'UniAlteri\Sellsy\Client\Exception\ErrorException' => 'Teknoo\Sellsy\Client\Exception\ErrorException',
+    'UniAlteri\Sellsy\Client\Collection\CollectionInterface' => 'Teknoo\Sellsy\Client\Collection\CollectionInterface',
+    'UniAlteri\Sellsy\Client\Collection\CollectionGeneratorInterface' => 'Teknoo\Sellsy\Client\Collection\CollectionGeneratorInterface',
+    'UniAlteri\Sellsy\Client\Collection\CollectionGenerator' => 'Teknoo\Sellsy\Client\Collection\CollectionGenerator',
+    'UniAlteri\Sellsy\Client\Collection\Collection' => 'Teknoo\Sellsy\Client\Collection\Collection',
+);
