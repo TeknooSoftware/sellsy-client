ccb4f3fc55da358090960a5de7cc94cb5e7cadce
Richard Deloge <richarddeloge@gmail.com>
2023-04-06 08:25:03 +0200

    Fix license header

--- tools/console.php
+++ tools/console.php
@@ -7,7 +7,7 @@
  * LICENSE
  *
  * This source file is subject to the MIT license
- * license that are bundled with this package in the folder licences
+ * that are bundled with this package in the folder licences
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to richard@teknoo.software so we can send you a copy immediately.
